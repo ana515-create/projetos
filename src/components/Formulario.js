06e9c86e7f6df1e8c6e169148d843f00a72b8bfc
ana-portela <anabeatrizportela897@gmail.com>
2025-09-03 16:29:43 -0300

    adicionas use states

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,22 +1,83 @@
+<<<<<<< HEAD
+import {useState} from "react"
+=======
+import { useState } from "react"
+>>>>>>> ccefc0d15e5ebea8bfb7e6f667a4a9ed000a0947
+
 function Formulario(){
 
     function cadastrarUsuario(e){
         e.preventDefault()
-        console.log('Cadastrou o usuario')
+<<<<<<< HEAD
+        //console.log(nome)
+=======
+        //console.log (nome)
+>>>>>>> ccefc0d15e5ebea8bfb7e6f667a4a9ed000a0947
+        //console.log('Cadastrou o usuário')
+        console.log(`Usuário ${nome} cadastrado com a senha: ${senha}`)
     }
+
+<<<<<<< HEAD
+    const [nome, setNome] = useState('Ana Portela')
+    const [senha, setSenha] = useState()
+=======
+    const [nome, setNome] = useState('Helo')
+    const [senha,setSenha] = useState()
+>>>>>>> ccefc0d15e5ebea8bfb7e6f667a4a9ed000a0947
+
     return(
         <div>
-          <h1>Cadastro</h1>
-          <form onSubmit={cadastrarUsuario}>
+            <h1>Cadastro</h1>
+        <form onSubmit={cadastrarUsuario}> 
             <div>
-                <input type="text" placeholder="Digite seu nome" />
+<<<<<<< HEAD
+                <label htmlFor="nome">Nome:</label>
+                <input type= "text" 
+                id="nome"
+                name="nome"
+                placeholder="Digite seu nome"
+                value={nome}
+                onChange={(e)=> setNome(e.target.value)}
 
+                  />
             </div>
             <div>
-                <input type="submit" value="Cadastrar" />
+                <label htmlFor="senha">Senha:</label>
+                <input type= "password"
+                 id="senha" 
+                 name="senha"
+                 placeholder="Digite sua senha"
+                 onChange={(e)=> setSenha(e.target.value)}
+
+                 
+                 />
+=======
+                <label htmlFor="nome">Nome: </label>
+                <input type= "text" 
+                id="nome"
+                name="nome" 
+                placeholder="Digite seu nome"
+                value={nome}
+                onChange={(e)=> setNome (e.target.value)}
+                />
             </div>
-          </form>
-        </div>
+            <div>
+                <label htmlFor="senha">Senha:</label>
+                <input 
+                type= "password" 
+                id="senha" 
+                name="senha" 
+                placeholder="Digite sua senha" 
+                onChange={(e)=> setSenha (e.target.value)}
+                />
+>>>>>>> ccefc0d15e5ebea8bfb7e6f667a4a9ed000a0947
+            </div>
+            <div>
+            <input type= "submit" value="Cadastrar"/>
+            </div>
+        </form>
+            </div>
     )
 }
-export default Formulario 
+
+export default Formulario
